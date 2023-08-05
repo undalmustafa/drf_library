44c2db30787e33611dc9575e2d0a8c864770a76f
kirvaland <undalmustafa@gmail.com>
2023-08-05 21:06:06 +0300

    implemented api with using django api framework

--- core/views.py
+++ core/views.py
@@ -1,11 +1,43 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework import permissions
-from .models import Book
-from .serializers import BookSerializer
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from core.models import Book
+from core.serializers import BookSerializer
 # Create your views here.
 
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+@csrf_exempt
+def book_list(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = BookSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+@csrf_exempt
+def book_detail(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = BookSerializer(book)
+        return JsonResponse(serializer.data)
+
+    elif request.method =="PUT":
+        data = JSONParser().parse(request)
+        serializer = BookSerializer(book, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method =='DELETE':
+        book.delete()
+        return HttpResponse(status=204)
