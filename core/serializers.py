81036f6ba07f71289540856e2ffceaa02ff19db8
kirvaland <undalmustafa@gmail.com>
2023-08-06 10:19:06 +0300

    added primary key to bookserializer

--- core/serializers.py
+++ core/serializers.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['book_name', "author_name", "book_genre"]
+        fields = ['pk', 'book_name', "author_name", "book_genre"]
