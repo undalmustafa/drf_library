47bc7e04f9f7da578c7cfeabd8e94a45835d9d7a
kirvaland <undalmustafa@gmail.com>
2023-08-08 19:57:33 +0300

    deleted unnecessary comments

--- core/models.py
+++ core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Book(models.Model):
     book_name = models.CharField(max_length=255)
     author_name = models.CharField(max_length=255)
