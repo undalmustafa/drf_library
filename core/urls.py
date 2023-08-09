1332d4aad21d1ad96d7911eee56f67ccac934f03
kirvaland <undalmustafa@gmail.com>
2023-08-09 12:54:45 +0300

    feat: implemented uuid and slug

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from core.views import BookItemsGet, BookItemsPost, BookDetail
 
 urlpatterns = [
-    path('books/get/', BookItemsGet.as_view()),
+    path('books/', BookItemsGet.as_view()),
     path('books/post/', BookItemsPost.as_view()),
-    path('books/<int:pk>/', BookDetail.as_view()),
+    path('books/<slug:slug>/', BookDetail.as_view()),
 ]
